b09cc1f10646a8dc5f4384a23025e30cd3fa11a5
MUHMDRZLL <rizalfahatrik@gmail.com>
2025-04-14 20:53:16 +0700

    Delete src directory

--- src/LibraryOperations.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.librarysystem;
-
-public interface LibraryOperations {
-    void addBook(Book book);
-    void viewBooks();
-    void borrowBook(int bookId, int userId);
-    void returnBook(int bookId);
-}
--- src/LibraryOperationsUser.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.librarysystem;
-
-public interface LibraryOperationsUser {
-    void addUse(User user);
-    void viewUsers();
-    void updateUser(int userId, String newName); // Sesuaikan kebutuhan
-    void borrowBook(int bookId, int userId);
-    void returnBook(int bookId);
-}
